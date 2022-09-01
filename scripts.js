274f68cf3a06b58ab0590e2f59a6cf7c92960bbd
Goldgamingyt <103109305+Goldgamingyt@users.noreply.github.com>
2022-08-31 22:06:52 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-$("#goal-button").click(function(){
-  $("#list-group").append("<li class='load'>" + $("#new-goal").val() + "</li>");
-  $("#new-goal").val("");
-  });
-});
